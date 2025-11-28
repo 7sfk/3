a826f0a34e1107cdb7d565c8c4c844788881a2ed
Your Name <you@example.com>
2025-11-28 07:11:09 +0000

    Clean project state with Firebase integration

--- ConstructAIApp.swift
+++ ConstructAIApp.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 @main
 struct ConstructAIApp: App {
+    // Connect the AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @StateObject private var appState = AppState()
 
     var body: some Scene {
